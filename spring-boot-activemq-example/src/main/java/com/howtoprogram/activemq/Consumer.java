ac7daee30fcd197e8dd9b1744c7e68dbb5ce7435
nangsaigon <binickname@gmail.com>
2017-09-21 18:12:58 +0700

    spring boot activemq example

---
+++ spring-boot-activemq-example/src/main/java/com/howtoprogram/activemq/Consumer.java
@@ -0,0 +1,14 @@
+package com.howtoprogram.activemq;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+	@JmsListener(destination = "hello.queue")
+	public void receiveQueue(String text) {
+		System.out.println(text);
+	}
+
+}
